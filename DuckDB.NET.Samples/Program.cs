4e004a4b0d944a314a70dc9a1af8caec0c778b70
Giorgi <giorgi@giorgi.dev>
2020-10-11 01:47:22 +0400

    Added project for samples

---
+++ DuckDB.NET.Samples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DuckDB.NET.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
