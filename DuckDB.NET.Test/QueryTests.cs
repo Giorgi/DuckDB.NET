1b268031ff5811ef61b8af8f7ff7c2232c6e6b3f
Giorgi Dalakishvili <giorgi@giorgi.dev>
2022-06-28 00:41:23 +0400

    Update tests

--- DuckDB.NET.Test/QueryTests.cs
+++ DuckDB.NET.Test/QueryTests.cs
@@ -1,4 +1,3 @@
-using DuckDB.NET;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,42 +9,42 @@ namespace DuckDB.NET.Test
         [TestMethod]
         public void Test()
         {
-            var result = PlatformIndependentBindings.NativeMethods.DuckDBOpen(null, out var database);
+            var result = NativeMethods.DuckDBOpen(null, out var database);
             result.Should().Be(DuckDBState.DuckDBSuccess);
 
-            result = PlatformIndependentBindings.NativeMethods.DuckDBConnect(database, out var connection);
+            result = NativeMethods.DuckDBConnect(database, out var connection);
             result.Should().Be(DuckDBState.DuckDBSuccess);
 
             using (database)
             using (connection)
             {
                 var table = "CREATE TABLE test(a INTEGER, b BOOLEAN);";
-                result = PlatformIndependentBindings.NativeMethods.DuckDBQuery(connection, table.ToUnmanagedString(), null);
+                result = NativeMethods.DuckDBQuery(connection, table.ToUnmanagedString(), null);
                 result.Should().Be(DuckDBState.DuckDBSuccess);
 
 
                 var queryResult = new DuckDBResult();
                 var insert = "INSERT INTO test VALUES (1, TRUE), (2, FALSE), (3, TRUE);";
-                result = PlatformIndependentBindings.NativeMethods.DuckDBQuery(connection, insert.ToUnmanagedString(), queryResult);
+                result = NativeMethods.DuckDBQuery(connection, insert.ToUnmanagedString(), queryResult);
                 result.Should().Be(DuckDBState.DuckDBSuccess);
 
-                var rowsChanged = PlatformIndependentBindings.NativeMethods.DuckDBRowsChanged(queryResult);
+                var rowsChanged = NativeMethods.DuckDBRowsChanged(queryResult);
                 rowsChanged.Should().Be(3);
 
-                PlatformIndependentBindings.NativeMethods.DuckDBDestroyResult(queryResult);
+                NativeMethods.DuckDBDestroyResult(queryResult);
 
 
                 var query = "SELECT * FROM test;";
-                result = PlatformIndependentBindings.NativeMethods.DuckDBQuery(connection, query.ToUnmanagedString(), queryResult);
+                result = NativeMethods.DuckDBQuery(connection, query.ToUnmanagedString(), queryResult);
                 result.Should().Be(DuckDBState.DuckDBSuccess);
 
-                var rowCount = PlatformIndependentBindings.NativeMethods.DuckDBRowCount(queryResult);
+                var rowCount = NativeMethods.DuckDBRowCount(queryResult);
                 rowCount.Should().Be(3);
 
-                var columnCount = PlatformIndependentBindings.NativeMethods.DuckDBColumnCount(queryResult);
+                var columnCount = NativeMethods.DuckDBColumnCount(queryResult);
                 columnCount.Should().Be(2);
 
-                PlatformIndependentBindings.NativeMethods.DuckDBDestroyResult(queryResult);
+                NativeMethods.DuckDBDestroyResult(queryResult);
             }
         }
     }
