5c2e73e461dfbf09857c688ea5a36e44a2a0f29b
Giorgi Dalakishvili <giorgi@giorgi.dev>
2022-12-19 01:32:28 +0400

    CI changes

--- DuckDB.NET.Test/ModuleInit.cs
+++ DuckDB.NET.Test/ModuleInit.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 #nullable enable
@@ -10,33 +9,43 @@ public static class ModuleInit
 	[ModuleInitializer]
 	public static void Init()
 	{
-		if (GetRid() is not { } rid) return;
-		if (NativeLibrary.TryLoad(Path.Join("runtimes", rid, "native", "duckdb"), out _)) return;
-		_ = NativeLibrary.TryLoad(Path.Join("runtimes", rid, "native", "libduckdb"), out _);
+        if (GetRid() is not { } rid)
+        {
+            return;
+        }
+
+        if (NativeLibrary.TryLoad(Path.Join("runtimes", rid, "native", "duckdb"), out _))
+        {
+            return;
+        }
 		
-	}
+        NativeLibrary.TryLoad(Path.Join("runtimes", rid, "native", "libduckdb"), out _);
+    }
 
 	private static string? GetRid()
 	{
-
-
 		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 		{
 			return Environment.Is64BitProcess 
 				? "win-x64" 
 				: "win-x86";
 		}
-		
-		else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-		{
-			return RuntimeInformation.ProcessArchitecture switch
-			{
-				Architecture.X64 => "linux-x64",
-				Architecture.Arm64 => "linux-arm64",
-				_ => null,
-			};
-		}
 
-		return null;
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            return RuntimeInformation.ProcessArchitecture switch
+            {
+                Architecture.X64 => "linux-x64",
+                Architecture.Arm64 => "linux-arm64",
+                _ => null,
+            };
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            return "osx-x64";
+        }
+
+        return null;
 	}
 }
