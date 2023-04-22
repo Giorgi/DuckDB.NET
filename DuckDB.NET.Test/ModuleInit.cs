dd66bad25b04b3c7ce0dc4f96369fdb5017e8b25
Giorgi Dalakishvili <giorgi@giorgi.dev>
2023-04-22 23:23:02 +0400

    Fix formatting

--- DuckDB.NET.Test/ModuleInit.cs
+++ DuckDB.NET.Test/ModuleInit.cs
@@ -3,13 +3,14 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
 #nullable enable
 namespace DuckDB.NET.Test;
 public static class ModuleInit
 {
-	[ModuleInitializer]
-	public static void Init()
-	{
+    [ModuleInitializer]
+    public static void Init()
+    {
         if (GetRid() is not { } rid)
         {
             return;
@@ -24,30 +25,30 @@ public static class ModuleInit
         {
             return;
         }
+    }
+
+    private static string? GetRid()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return Environment.Is64BitProcess ? "win-x64" : "win-x86";
         }
 
-        private static string? GetRid()
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
         {
-	    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-	    {
-		    return Environment.Is64BitProcess ? "win-x64" : "win-x86";
-	    }
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            return RuntimeInformation.ProcessArchitecture switch
             {
-                return RuntimeInformation.ProcessArchitecture switch
-                {
-                    Architecture.X64 => "linux-x64",
-                    Architecture.Arm64 => "linux-arm64",
-                    _ => null,
-                };
-            }
+                Architecture.X64 => "linux-x64",
+                Architecture.Arm64 => "linux-arm64",
+                _ => null,
+            };
+        }
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                return "osx";
-            }
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            return "osx";
+        }
 
-            return null;
-	}
+        return null;
+    }
 }
