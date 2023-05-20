cc5fbf2698c37cf8d413ac4ca86fe8051eac499f
Giorgi Dalakishvili <giorgi@giorgi.dev>
2023-05-20 22:42:54 +0400

    Remove extra constructors

--- DuckDB.NET.Data/DuckDBException.cs
+++ DuckDB.NET.Data/DuckDBException.cs
@@ -1,34 +1,25 @@
-﻿using System;
-using System.Data.Common;
+﻿using System.Data.Common;
 using System.Runtime.Serialization;
 
 namespace DuckDB.NET.Data
 {
     public class DuckDBException : DbException
     {
-        public DuckDBException()
+        internal DuckDBException()
         {
         }
 
-        public DuckDBException(SerializationInfo info, StreamingContext context) : base(info, context)
+        internal DuckDBException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
 
-        public DuckDBException(string message) : base(message)
+        internal DuckDBException(string message) : base(message)
         {
         }
 
-        public DuckDBException(string message, Exception innerException) : base(message, innerException)
+        internal DuckDBException(string message, DuckDBState state) : base(message, (int)state)
         {
-        }
 
-        public DuckDBException(string message, int errorCode) : base(message, errorCode)
-        {
-        }
-
-        public DuckDBException(string message, DuckDBState state): base(message, (int)state)
-        {
-            
         }
     }
 }
