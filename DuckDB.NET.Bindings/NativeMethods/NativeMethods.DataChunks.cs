a397761f5ed03e9c1be1a7003ff45ca8eb7e6716
Kristian Mosegaard <kristian@mosen.me>
2023-09-17 14:26:14 +0200

    Return DuckDBLogicalType instead of IntPtr.

--- DuckDB.NET.Bindings/NativeMethods/NativeMethods.DataChunks.cs
+++ DuckDB.NET.Bindings/NativeMethods/NativeMethods.DataChunks.cs
@@ -17,7 +17,7 @@ public partial class NativeMethods
         public static extern long DuckDBDataChunkGetSize(DuckDBDataChunk chunk);
 
         [DllImport(DuckDbLibrary, CallingConvention = CallingConvention.Cdecl, EntryPoint = "duckdb_vector_get_column_type")]
-        public static extern IntPtr DuckDBVectorGetColumnType(IntPtr vector);
+        public static extern DuckDBLogicalType DuckDBVectorGetColumnType(IntPtr vector);
 
         [DllImport(DuckDbLibrary, CallingConvention = CallingConvention.Cdecl, EntryPoint = "duckdb_vector_get_data")]
         public static extern IntPtr DuckDBVectorGetData(IntPtr vector);
