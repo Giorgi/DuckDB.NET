8c3e096f069a54c4f5eea1025163eab568a3ccd1
Kristian Mosegaard <kristian@mosen.me>
2023-09-17 15:26:54 +0200

    DuckDBGetColumnCount should take a DuckDBDataChunk.

--- DuckDB.NET.Bindings/NativeMethods/NativeMethods.DataChunks.cs
+++ DuckDB.NET.Bindings/NativeMethods/NativeMethods.DataChunks.cs
@@ -8,7 +8,7 @@ public partial class NativeMethods
     public static class DataChunks
     {
         [DllImport(DuckDbLibrary, CallingConvention = CallingConvention.Cdecl, EntryPoint = "duckdb_data_chunk_get_column_count")]
-        public static extern long DuckDBDataChunkGetColumnCount(IntPtr chunk);
+        public static extern long DuckDBDataChunkGetColumnCount(DuckDBDataChunk chunk);
 
         [DllImport(DuckDbLibrary, CallingConvention = CallingConvention.Cdecl, EntryPoint = "duckdb_data_chunk_get_vector")]
         public static extern IntPtr DuckDBDataChunkGetVector(DuckDBDataChunk chunk, long columnIndex);
