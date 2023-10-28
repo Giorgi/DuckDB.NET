d3ef53eacc7a460c1cdbd30de0ce34dfeb627cbf
Cédric L. Charlier <seddryck@gmail.com>
2023-10-28 10:10:07 +0200

    perf: remove boxing/unboxing for List

--- DuckDB.NET.Data/TypeHandlers/ListTypeHandler.cs
+++ DuckDB.NET.Data/TypeHandlers/ListTypeHandler.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data.SqlTypes;
 using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
+using static System.Collections.Specialized.BitVector32;
 
 namespace DuckDB.NET.Data.TypeHandlers
 {
@@ -12,6 +16,9 @@ namespace DuckDB.NET.Data.TypeHandlers
     {
         private ITypeHandler InternalTypeHandler { get; }
         private DuckDBLogicalType LogicalType { get; }
+
+        private KeyValuePair<Type, Delegate> ListCache { get; set; } = new();
+        private KeyValuePair<Type, Delegate> ItemCache { get; set; } = new();
 
         public override Type ClrType => typeof(List<>).MakeGenericType(InternalTypeHandler.ClrType);
 
@@ -32,57 +39,84 @@ namespace DuckDB.NET.Data.TypeHandlers
         public override object GetValue(ulong offset)
         {
             var listType = typeof(List<>).MakeGenericType(InternalTypeHandler.ClrType);
-            return GetList(offset, listType);
+            return GetValue(offset, listType);
         }
 
         public override object GetValue(ulong offset, Type type)
         {
             var listType = typeof(List<>).MakeGenericType(type.GetGenericArguments()[0]);
-            var value = GetList(offset, listType);
-            var converted = Convert(value, type);
-            return converted;
+            if (ListCache.Key != listType)
+            {
+                var methodInfo = typeof(ListTypeHandler)
+                                            .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                                            .Where(x => x.Name==nameof(GetValue))
+                                            .Where(x => x.GetParameters().Length==1)
+                                            .Where(x => x.ContainsGenericParameters)
+                                            .First()
+                                            .MakeGenericMethod(new[] { listType });
+
+                var param = Expression.Parameter(typeof(ulong));
+                var callRef = Expression.Call(Expression.Constant(this), methodInfo, param);
+                var lambda = Expression.Lambda(callRef, new[] { param });
+                var compiled = lambda.Compile();
+                ListCache = new(listType, compiled);
+            }
+            var expression = (Func<ulong, object>)ListCache.Value;
+            var value = expression.Invoke(offset);
+            //var converted = Convert(value, type);
+            //return converted;
+            return value;
         }
 
         public override T GetValue<T>(ulong offset)
-            => (T)GetList(offset, typeof(T));
-
-        public T GetList<T>(ulong offset) where T : IList
-            => (T)GetList(offset, typeof(T));
-
-        internal unsafe object GetList(ulong offset, Type returnType)
         {
-            if (!typeof(IList).IsAssignableFrom(returnType))
-            {
-                throw new InvalidOperationException($"Cannot return a list with a {nameof(returnType)} not implementing IList.");
+            if (ItemCache.Key != typeof(T))
+            { 
+                if (!typeof(IList).IsAssignableFrom(typeof(T)))
+                    throw new InvalidOperationException($"Cannot return a list with a {nameof(T)} not implementing IList.");
+
+                var genericArgs = typeof(T).GetGenericArguments();
+                if (genericArgs.Length == 0)
+                    throw new InvalidOperationException($"Cannot return a list with a {nameof(T)} not implementing IList<>.");
+                var listType = genericArgs[0];
+
+                var nullableType = Nullable.GetUnderlyingType(listType);
+                var allowNulls = !listType.IsValueType || nullableType != null;
+                var targetType = nullableType ?? listType;
+
+                var methodInfo = typeof(ListTypeHandler)
+                                    .GetMethod(nameof(GetList), BindingFlags.Instance | BindingFlags.NonPublic)!
+                                    .MakeGenericMethod(new[] { typeof(T), listType });
+
+                var param = Expression.Parameter(typeof(ulong));
+                var callRef = Expression.Call(Expression.Constant(this), methodInfo, param, Expression.Constant(allowNulls));
+                var lambda = Expression.Lambda(callRef, new[] { param });
+                var compiled = lambda.Compile();
+                ItemCache = new (targetType, compiled);
             }
+            var expression = (Func<ulong, T>)ItemCache.Value;
+            var value = expression.Invoke(offset);
+            return value;
+        }
 
-            var genericArgs = returnType.GetGenericArguments();
-            if (genericArgs.Length == 0)
-                throw new InvalidOperationException($"Cannot return a list with a {nameof(returnType)} not implementing IList<>.");
-            var listType = genericArgs[0];
-
+        protected unsafe T GetList<T, U>(ulong offset, bool allowNulls) where T : IList<U?>
+        {
             var listData = (DuckDBListEntry*)DataPointer + offset;
+            var list = Activator.CreateInstance<T>()!;
             
-            var nullableType = Nullable.GetUnderlyingType(listType);
-            var allowNulls = !listType.IsValueType || nullableType != null;
-
-            var list = (IList)Activator.CreateInstance(returnType)!;
-
-            var targetType = nullableType ?? listType;
-
             for (ulong i = 0; i < listData->Length; i++)
             {
                 var childOffset = i + listData->Offset;
                 if (InternalTypeHandler.IsValid(childOffset))
                 {
-                    var item = InternalTypeHandler.GetValue(childOffset, targetType);
+                    var item = InternalTypeHandler.GetValue<U>(childOffset);
                     list.Add(item);
                 }
                 else
                 {
                     if (allowNulls)
                     {
-                        list.Add(null);
+                        list.Add(default);
                     }
                     else
                     {
@@ -90,7 +124,6 @@ namespace DuckDB.NET.Data.TypeHandlers
                     }
                 }
             }
-
             return list;
         }
 
