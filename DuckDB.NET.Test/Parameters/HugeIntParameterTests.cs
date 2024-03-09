3aaef840eeed917f1533401c4465fa1934135982
Giorgi <giorgi@giorgi.dev>
2024-03-09 15:32:41 +0400

    Negative HugeInt tests

--- DuckDB.NET.Test/Parameters/HugeIntParameterTests.cs
+++ DuckDB.NET.Test/Parameters/HugeIntParameterTests.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using DuckDB.NET.Data;
+using DuckDB.NET.Native;
 using FluentAssertions;
 using Xunit;
 
@@ -36,16 +37,52 @@ public class HugeIntParameterTests(DuckDBDatabaseFixture db) : DuckDBTestBase(db
         Command.ExecuteNonQuery();
 
         Command.CommandText = "INSERT INTO HugeIntTests VALUES (9, ?);";
-        
+
         var value = BigInteger.Add(ulong.MaxValue, 125);
         Command.Parameters.Add(new DuckDBParameter(value));
         Command.ExecuteNonQuery();
 
         Command.CommandText = "SELECT * from HugeIntTests;";
-        
+
         var reader = Command.ExecuteReader();
         reader.Read();
-        
+
+        var receivedValue = reader.GetFieldValue<BigInteger>(1);
+        receivedValue.Should().Be(value);
+    }
+
+    [Fact]
+    public void SimpleNegativeHugeIntTest()
+    {
+        Command.CommandText = $"SELECT {DuckDBHugeInt.HugeIntMinValue}::HUGEINT;";
+        Command.ExecuteNonQuery();
+
+        var scalar = Command.ExecuteScalar();
+        scalar.Should().Be(DuckDBHugeInt.HugeIntMinValue);
+
+        var reader = Command.ExecuteReader();
+        reader.Read();
+        var receivedValue = reader.GetFieldValue<BigInteger>(0);
+        receivedValue.Should().Be(DuckDBHugeInt.HugeIntMinValue);
+    }
+
+    [Fact]
+    public void BindNegativeHugeIntValueTest()
+    {
+        Command.CommandText = "CREATE TABLE NegativeHugeIntTests (key INTEGER, value HugeInt)";
+        Command.ExecuteNonQuery();
+
+        Command.CommandText = "INSERT INTO NegativeHugeIntTests VALUES (9, ?);";
+
+        var value = DuckDBHugeInt.HugeIntMinValue;
+        Command.Parameters.Add(new DuckDBParameter(value));
+        Command.ExecuteNonQuery();
+
+        Command.CommandText = "SELECT * from NegativeHugeIntTests;";
+
+        var reader = Command.ExecuteReader();
+        reader.Read();
+
         var receivedValue = reader.GetFieldValue<BigInteger>(1);
         receivedValue.Should().Be(value);
     }
