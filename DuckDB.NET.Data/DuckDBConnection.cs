d3336905d7ce4784df3a8e01c0a7eb80137aabed
Alex Zaytsev <hazzik@gmail.com>
2024-05-20 18:45:39 +1000

    Implement Connection.GetSchema (#185)
    * WIP Connection.GetSchema
    
    * Code cleanup
    
    * Implement ReservedWords
    
    * Ooops
    
    * Fix tests
    
    * Add test for ArgumentOutOfRangeException
    
    * Refactoring
    
    * Use DbMetaDataCollectionNames && DbMetaDataColumnNames where possible
    
    * Remove redundant code
    
    * Implement Columns schema
    
    * Add Columns schema metadata
    
    * Add ForeignKeys schema
    
    * Implement Indexes metadata
    
    * Fix indexes test
    
    * Use constant
    
    * Extract restrictions arrays

--- DuckDB.NET.Data/DuckDBConnection.cs
+++ DuckDB.NET.Data/DuckDBConnection.cs
@@ -215,4 +215,13 @@ public class DuckDBConnection : DbConnection
 
         return duplicatedConnection;
     }
+
+    public override DataTable GetSchema() =>
+        GetSchema(DbMetaDataCollectionNames.MetaDataCollections);
+
+    public override DataTable GetSchema(string collectionName) =>
+        GetSchema(collectionName, null);
+
+    public override DataTable GetSchema(string collectionName, string?[]? restrictionValues) =>
+        DuckDBSchema.GetSchema(this, collectionName, restrictionValues);
 }
