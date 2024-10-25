9e14d36a38b458b85399b5055c2207d5dd6f2ad8
Giorgi Dalakishvili <giorgi@giorgi.dev>
2024-10-25 12:34:41 +0400

    Reorder switch statements

--- DuckDB.NET.Data/Internal/ClrToDuckDBConverter.cs
+++ DuckDB.NET.Data/Internal/ClrToDuckDBConverter.cs
@@ -13,30 +13,36 @@ internal static class ClrToDuckDBConverter
         value switch
         {
             bool val => NativeMethods.Value.DuckDBCreateBool(val),
+            
             sbyte val => NativeMethods.Value.DuckDBCreateInt8(val),
             short val => NativeMethods.Value.DuckDBCreateInt16(val),
             int val => NativeMethods.Value.DuckDBCreateInt32(val),
+            long val => NativeMethods.Value.DuckDBCreateInt64(val),
+            
             byte val => NativeMethods.Value.DuckDBCreateUInt8(val),
             ushort val => NativeMethods.Value.DuckDBCreateUInt16(val),
             uint val => NativeMethods.Value.DuckDBCreateUInt32(val),
-            long val => NativeMethods.Value.DuckDBCreateInt64(val),
             ulong val => NativeMethods.Value.DuckDBCreateUInt64(val),
+            
             float val => NativeMethods.Value.DuckDBCreateFloat(val),
             double val => NativeMethods.Value.DuckDBCreateDouble(val),
-            string val => StringToDuckDBValue(val),
+            
             decimal val => DecimalToDuckDBValue(val),
-            Guid val => GuidToDuckDBValue(val),
             BigInteger val => NativeMethods.Value.DuckDBCreateHugeInt(new DuckDBHugeInt(val)),
-            byte[] val => NativeMethods.Value.DuckDBCreateBlob(val, val.Length),
-            TimeSpan val => NativeMethods.Value.DuckDBCreateInterval(val),
+            
+            string val => StringToDuckDBValue(val),
+            Guid val => GuidToDuckDBValue(val),
             DateTime val => NativeMethods.Value.DuckDBCreateTimestamp(NativeMethods.DateTimeHelpers.DuckDBToTimestamp(DuckDBTimestamp.FromDateTime(val))),
-            DateTimeOffset val => DateTimeOffsetToDuckDBValue(val),
+            TimeSpan val => NativeMethods.Value.DuckDBCreateInterval(val),
             DuckDBDateOnly val => NativeMethods.Value.DuckDBCreateDate(NativeMethods.DateTimeHelpers.DuckDBToDate(val)),
             DuckDBTimeOnly val => NativeMethods.Value.DuckDBCreateTime(NativeMethods.DateTimeHelpers.DuckDBToTime(val)),
 #if NET6_0_OR_GREATER
             DateOnly val => NativeMethods.Value.DuckDBCreateDate(NativeMethods.DateTimeHelpers.DuckDBToDate(val)),
             TimeOnly val => NativeMethods.Value.DuckDBCreateTime(NativeMethods.DateTimeHelpers.DuckDBToTime(val)),
 #endif
+            DateTimeOffset val => DateTimeOffsetToDuckDBValue(val),
+            byte[] val => NativeMethods.Value.DuckDBCreateBlob(val, val.Length),
+            
             ICollection val => CreateCollectionValue(val),
             _ => throw new InvalidCastException($"Cannot convert value of type {value.GetType().FullName} to DuckDBValue.")
         };
@@ -73,21 +79,24 @@ internal static class ClrToDuckDBConverter
             ICollection<bool> items => CreateCollectionValue(DuckDBType.Boolean, items),
 
             ICollection<sbyte> items => CreateCollectionValue(DuckDBType.TinyInt, items),
-            ICollection<byte> items => CreateCollectionValue(DuckDBType.UnsignedTinyInt, items),
             ICollection<short> items => CreateCollectionValue(DuckDBType.SmallInt, items),
-            ICollection<ushort> items => CreateCollectionValue(DuckDBType.UnsignedSmallInt, items),
             ICollection<int> items => CreateCollectionValue(DuckDBType.Integer, items),
-            ICollection<uint> items => CreateCollectionValue(DuckDBType.UnsignedInteger, items),
             ICollection<long> items => CreateCollectionValue(DuckDBType.BigInt, items),
+
+            ICollection<byte> items => CreateCollectionValue(DuckDBType.UnsignedTinyInt, items),
+            ICollection<ushort> items => CreateCollectionValue(DuckDBType.UnsignedSmallInt, items),
+            ICollection<uint> items => CreateCollectionValue(DuckDBType.UnsignedInteger, items),
             ICollection<ulong> items => CreateCollectionValue(DuckDBType.UnsignedBigInt, items),
+            
             ICollection<float> items => CreateCollectionValue(DuckDBType.Float, items),
             ICollection<double> items => CreateCollectionValue(DuckDBType.Double, items),
-            ICollection<BigInteger> items => CreateCollectionValue(DuckDBType.HugeInt, items),
+            
             ICollection<decimal> items => CreateCollectionValue(DuckDBType.Varchar, items),
-            ICollection<Guid> items => CreateCollectionValue(DuckDBType.Varchar, items),
+            ICollection<BigInteger> items => CreateCollectionValue(DuckDBType.HugeInt, items),
+            
             ICollection<string> items => CreateCollectionValue(DuckDBType.Varchar, items),
+            ICollection<Guid> items => CreateCollectionValue(DuckDBType.Varchar, items),
             ICollection<DateTime> items => CreateCollectionValue(DuckDBType.Date, items),
-            ICollection<DateTimeOffset> items => CreateCollectionValue(DuckDBType.TimeTz, items),
             ICollection<TimeSpan> items => CreateCollectionValue(DuckDBType.Interval, items),
             ICollection<DuckDBDateOnly> items => CreateCollectionValue(DuckDBType.Date, items),
             ICollection<DuckDBTimeOnly> items => CreateCollectionValue(DuckDBType.Time, items),
@@ -95,6 +104,7 @@ internal static class ClrToDuckDBConverter
             ICollection<DateOnly> items => CreateCollectionValue(DuckDBType.Date, items),
             ICollection<TimeOnly> items => CreateCollectionValue(DuckDBType.Time, items),
 #endif
+            ICollection<DateTimeOffset> items => CreateCollectionValue(DuckDBType.TimeTz, items),
             _ => throw new InvalidOperationException($"Cannot convert collection type {collection.GetType().FullName} to DuckDBValue.")
         };
     }
