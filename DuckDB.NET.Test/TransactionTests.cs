a3e0f54f5d63b7ef1b09099e4a0cdd74c5708eb6
Calum Sieppert <sieppertcalum@gmail.com>
2025-11-17 11:38:03 -0700

    Failing test for transaction abortion handling (#301)

--- DuckDB.NET.Test/TransactionTests.cs
+++ DuckDB.NET.Test/TransactionTests.cs
@@ -113,4 +113,42 @@ public class TransactionTests(DuckDBDatabaseFixture db) : DuckDBTestBase(db)
         Connection.Invoking(connection => connection.BeginTransaction(IsolationLevel.Serializable)).Should()
             .Throw<ArgumentException>();
     }
+
+    [Fact]
+    public void AbortedTransactionTest()
+    {
+        // This block of code is to make the transaction commit fail using an index limitation in duckdb
+        // (https://github.com/duckdb/duckdb/issues/17802)
+        Command.CommandText = "CREATE TABLE IF NOT EXISTS test_table (id INTEGER PRIMARY KEY);";
+        Command.ExecuteNonQuery();
+        Command.CommandText = "INSERT OR IGNORE INTO test_table VALUES (1);";
+        Command.ExecuteNonQuery();
+        // Keep a reference to the row in an open transaction to trigger the concurrent access limitation
+        using var tx1 = Connection.BeginTransaction();
+        Command.Transaction = tx1;
+        Command.CommandText = "SELECT id FROM test_table LIMIT 1;";
+        using var reader1 = Command.ExecuteReader();
+        using var conn2 = Connection.Duplicate();
+        conn2.Open();
+        using var cmd2 = conn2.CreateCommand();
+        cmd2.CommandText = "UPDATE test_table SET id = 1 WHERE id = 1;";
+        cmd2.ExecuteNonQuery();
+        var tx2 = conn2.BeginTransaction();
+        cmd2.Transaction = tx2;
+        cmd2.CommandText = "UPDATE test_table SET id = 1 WHERE id = 1;";
+        cmd2.ExecuteNonQuery();
+
+        using (new FluentAssertions.Execution.AssertionScope())
+        {
+            // Check that when the transaction commit fails and the transaction
+            // enters an aborted state, the transaction and connection objects
+            // remain in the expected state.
+            tx2.Invoking(tx2 => tx2.Commit()).Should().Throw<DuckDBException>().Where(ex => ex.ErrorType == Native.DuckDBErrorType.Transaction);
+            tx2.Invoking(tx2 => tx2.Commit()).Should().Throw<InvalidOperationException>();
+            tx2.Invoking(tx2 => tx2.Rollback()).Should().Throw<InvalidOperationException>();
+            tx2.Invoking(tx2 => tx2.Dispose()).Should().NotThrow();
+
+            conn2.Invoking(conn2 => conn2.BeginTransaction()).Should().NotThrow();
+        }
+    }
 }
