887f8298ac32acab693c99b606fd4ec07f4f0adf
Calum Sieppert <sieppertcalum@gmail.com>
2025-12-01 12:08:49 -0700

    Fix duckdb_*_is_finite marshalling

--- DuckDB.NET.Test/DuckDBDataReaderInfinityTests.cs
+++ DuckDB.NET.Test/DuckDBDataReaderInfinityTests.cs
@@ -9,6 +9,71 @@ namespace DuckDB.NET.Test;
 public class DuckDBDataReaderInfinityTests(DuckDBDatabaseFixture db) : DuckDBTestBase(db)
 {
     [Fact]
+    public void duckdb_date_is_finite()
+    {
+        var positiveInfinity = new DuckDBDate { Days = Int32.MaxValue };
+        var negativeInfinity = new DuckDBDate { Days = -Int32.MaxValue }; // NOTE: -MaxValue, not MinValue
+        var finitePositive = new DuckDBDate { Days = Int32.MaxValue - 1 };
+        var finiteNegative = new DuckDBDate { Days = -Int32.MaxValue + 1 };
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteDate(finitePositive).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteDate(positiveInfinity).Should().BeFalse();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteDate(finiteNegative).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteDate(negativeInfinity).Should().BeFalse();
+    }
+
+    [Fact]
+    public void duckdb_timestamp_is_finite()
+    {
+        var positiveInfinity = new DuckDBTimestampStruct { Micros = Int64.MaxValue };
+        var negativeInfinity = new DuckDBTimestampStruct { Micros = -Int64.MaxValue };
+        var finitePositive = new DuckDBTimestampStruct { Micros = Int64.MaxValue - 1 };
+        var finiteNegative = new DuckDBTimestampStruct { Micros = -Int64.MaxValue + 1 };
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestamp(finitePositive).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestamp(positiveInfinity).Should().BeFalse();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestamp(finiteNegative).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestamp(negativeInfinity).Should().BeFalse();
+    }
+
+    [Fact]
+    public void duckdb_timestamp_s_is_finite()
+    {
+        var positiveInfinity = new DuckDBTimestampStruct { Micros = Int64.MaxValue };
+        var negativeInfinity = new DuckDBTimestampStruct { Micros = -Int64.MaxValue };
+        var finitePositive = new DuckDBTimestampStruct { Micros = Int64.MaxValue - 1 };
+        var finiteNegative = new DuckDBTimestampStruct { Micros = -Int64.MaxValue + 1 };
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampS(finitePositive).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampS(positiveInfinity).Should().BeFalse();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampS(finiteNegative).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampS(negativeInfinity).Should().BeFalse();
+    }
+
+    [Fact]
+    public void duckdb_timestamp_ms_is_finite()
+    {
+        var positiveInfinity = new DuckDBTimestampStruct { Micros = Int64.MaxValue };
+        var negativeInfinity = new DuckDBTimestampStruct { Micros = -Int64.MaxValue };
+        var finitePositive = new DuckDBTimestampStruct { Micros = Int64.MaxValue - 1 };
+        var finiteNegative = new DuckDBTimestampStruct { Micros = -Int64.MaxValue + 1 };
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampMs(finitePositive).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampMs(positiveInfinity).Should().BeFalse();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampMs(finiteNegative).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampMs(negativeInfinity).Should().BeFalse();
+    }
+
+    [Fact]
+    public void duckdb_timestamp_ns_is_finite()
+    {
+        var positiveInfinity = new DuckDBTimestampStruct { Micros = Int64.MaxValue };
+        var negativeInfinity = new DuckDBTimestampStruct { Micros = -Int64.MaxValue };
+        var finitePositive = new DuckDBTimestampStruct { Micros = Int64.MaxValue - 1 };
+        var finiteNegative = new DuckDBTimestampStruct { Micros = -Int64.MaxValue + 1 };
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampNs(finitePositive).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampNs(positiveInfinity).Should().BeFalse();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampNs(finiteNegative).Should().BeTrue();
+        NativeMethods.DateTimeHelpers.DuckDBIsFiniteTimestampNs(negativeInfinity).Should().BeFalse();
+    }
+
+    [Fact]
     public void ReadInfinityDate()
     {
         Command.CommandText = "SELECT 'infinity'::DATE";
@@ -55,23 +120,35 @@ public class DuckDBDataReaderInfinityTests(DuckDBDatabaseFixture db) : DuckDBTes
     [Fact]
     public void ReadInfinityTimestamp()
     {
-        Command.CommandText = "SELECT 'infinity'::TIMESTAMP";
+        Command.CommandText = "SELECT 'infinity'::TIMESTAMP, 'infinity'::TIMESTAMP_NS, 'infinity'::TIMESTAMP_MS, 'infinity'::TIMESTAMP_S";
         using var reader = Command.ExecuteReader();
         reader.Read();
 
         reader.GetDateTime(0).Should().Be(DateTime.MaxValue);
         reader.GetFieldValue<DateTime>(0).Should().Be(DateTime.MaxValue);
+        reader.GetDateTime(1).Should().Be(DateTime.MaxValue);
+        reader.GetFieldValue<DateTime>(1).Should().Be(DateTime.MaxValue);
+        reader.GetDateTime(2).Should().Be(DateTime.MaxValue);
+        reader.GetFieldValue<DateTime>(2).Should().Be(DateTime.MaxValue);
+        reader.GetDateTime(3).Should().Be(DateTime.MaxValue);
+        reader.GetFieldValue<DateTime>(3).Should().Be(DateTime.MaxValue);
     }
 
     [Fact]
     public void ReadNegativeInfinityTimestamp()
     {
-        Command.CommandText = "SELECT '-infinity'::TIMESTAMP";
+        Command.CommandText = "SELECT '-infinity'::TIMESTAMP, '-infinity'::TIMESTAMP_NS, '-infinity'::TIMESTAMP_MS, '-infinity'::TIMESTAMP_S";
         using var reader = Command.ExecuteReader();
         reader.Read();
 
         reader.GetDateTime(0).Should().Be(DateTime.MinValue);
         reader.GetFieldValue<DateTime>(0).Should().Be(DateTime.MinValue);
+        reader.GetDateTime(1).Should().Be(DateTime.MinValue);
+        reader.GetFieldValue<DateTime>(1).Should().Be(DateTime.MinValue);
+        reader.GetDateTime(2).Should().Be(DateTime.MinValue);
+        reader.GetFieldValue<DateTime>(2).Should().Be(DateTime.MinValue);
+        reader.GetDateTime(3).Should().Be(DateTime.MinValue);
+        reader.GetFieldValue<DateTime>(3).Should().Be(DateTime.MinValue);
     }
 
     [Fact]
